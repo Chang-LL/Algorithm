64a9cecf5e32aa9361d43db74f5e7dd6d41edc42
cl <13546417565@163.com>
2017-12-21 22:40:00 +0800

    哈密顿环

--- Algorithm/MGraph.cpp
+++ Algorithm/MGraph.cpp
@@ -82,6 +82,7 @@ void MGraph<T>::Floyd(T **& d, int **& path)
 template<typename T>
 void MGraph<T>::mColoring(int m, int * x)
 {
+	mColoring(0, m, x);
 }
 
 
@@ -106,17 +107,18 @@ int MGraph<T>::Choose(int* d, bool* s)
 
 template<typename T>
 void MGraph<T>::NextValue(int k, int m, int * x)
-{
+{//在[1:m]中为x[k]确定一个最小的且不与其邻接点冲突的颜色
+	//x[k]=0表示没有可用颜色，颜色从1开始编号
 	int j;
 	do {
-		x[k] = (x[k] + 1) % (m + 1);
-		if (!x[k])return;
+		x[k] = (x[k] + 1) % (m + 1);//尝试下一种颜色
+		if (!x[k])return;//没有可用颜色
 		for (j = 0; j < k; j++)
 		{
-			if (a[k][j] && x[k] == x[j])
-				break;
+			if (a[k][j] && x[k] == x[j])//(i,j)是图的边，且相邻节点k和j颜色相同
+				break;//冲突，选下一种颜色
 		}
-		if (j == k)return;
+		if (j == k)return;//成功选择，返回
 	} while (1);
 }
 
@@ -124,14 +126,56 @@ template<typename T>
 void MGraph<T>::mColoring(int k, int m, int * x)
 {
 	do {
-		NextValue(k, m, x);
-		if (!x[k])break;
-		if (k == n - 1)
+		NextValue(k, m, x);//为x[k]分配颜色
+		if (!x[k])break;//x[k]=0表示没有合适的颜色
+		if (k == n - 1)//得到一种m-着色方案
 		{
 			for (int i = 0; i < n; i++)
 				std::cout << x[i] << ' ';
 			std::cout << std::endl;
 		}
-		else mColoring(k + 1, m, x);
+		else mColoring(k + 1, m, x);//已经对前k个节点分配颜色，尝试其余节点
 	} while (true);
+}
+
+// 哈密顿环算法，约束函数
+template<typename T>
+void MGraph<T>::NextValue(int k, int* x)
+{
+	do {
+		x[k] = (x[k] + 1) % n;//下一个节点
+		if (!x[k])return;
+		if (a[x[k - 1]][x[k]]) {//(x[k-1],x[k]是否为边
+			for (int j = 0; j < k; j++)//检查与前k个节点是否相同
+				if (x[j] == x[k])
+					break;//重复
+			if (j == k)//可取
+				if ((k < n - 1) || (k == n - 1) && a[x[n - 1]][x[0]])
+					return;
+		}
+	} while (true);
+}
+
+// 实际递归计算哈密顿环
+template<typename T>
+void MGraph<T>::Hamiltonian(int k, int* x)
+{
+	do {
+		NextValue(k, x);
+		if (!x[k])return;
+		if (k == n - 1) {
+			for (int i = 0; i < n; i++)
+			{
+				std::cout << x[i] << " ";
+			}
+			std::cout << "0\n";
+		}
+		else Hamiltonian(k + 1, x);
+	} while (true);
+}
+
+template<typename T>
+void MGraph<T>::Hamiltonian(int* x)
+{
+	Hamiltonian(1, x);
 }
--- Algorithm/MGraph.h
+++ Algorithm/MGraph.h
@@ -19,7 +19,16 @@ private:
 	// 在一维数组里求最小值
 	int Choose(int* d, bool* s);
 	//图着色
+	//对所有i和j，若a[i][j]=1,则xi！=xj
 	void NextValue(int k, int m, int *x);
 	void mColoring(int k, int m, int*x);
+	//哈密顿环算法,所有顶点的回路
+	//xi!=xj(i!=j) (xi,xi+1)属于E,xi,xi+1属于V,(xn-1,x0)属于E
+	//约束函数
+	void NextValue(int k, int* x);
+	// 实际递归计算哈密顿环
+	void Hamiltonian(int k, int* x);
+public:
+	void Hamiltonian(int* x);
 };
 
